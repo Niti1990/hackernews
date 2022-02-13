18962bc7f1dc034618b73bd0f30bda3bcf843213
nitika <nitikadiwakar.bajaj@gmail.com>
2022-02-13 19:52:15 +0100

    changes in header

--- hackernews/src/components/Header.js
+++ hackernews/src/components/Header.js
@@ -2,9 +2,12 @@ import React from 'react';
 function Header() {
     return (
         <div>
+          
             <h1>HACKER NEWS</h1>
+            <ul className='nav'>
             <h3 className='header'>New</h3>
-            <input className='header' type='text' placeholder='Enter' />
+            <input className='input' type='text' placeholder='Enter' />
+            </ul>
         </div>
     );
 }
