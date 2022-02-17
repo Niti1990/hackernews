3e673bb230d0a3ef1d911b65aa29165b76ab3a8b
Saritha Sujith <pillai.sujith.saritha@gmail.com>
2022-02-17 23:21:55 +0100

    changes in css

--- hackernews/src/components/List.js
+++ hackernews/src/components/List.js
@@ -3,48 +3,51 @@ import SearchIcon from '@mui/icons-material/Search';
 
 //import {Link} from 'react-router-dom';
 function List({ hits }) {
+	const [filterhits, setFilterhits] = useState([]);
 
-  const [filterhits, setFilterhits] = useState([]);
+	const handleFilter = (event) => {
+		const searchWord = event.target.value;
+		const newFilter = hits.filter((value) => {
+			return value.title.toLowerCase().includes(searchWord.toLowerCase());
+		});
 
-   const handleFilter =(event) =>{
-       const searchWord =event.target.value
-       const newFilter = hits.filter((value) => { 
-     return value.title.toLowerCase().includes(searchWord.toLowerCase());
-});
- 
- setFilterhits(newFilter);
-   };
+		setFilterhits(newFilter);
+	};
 
-    //const hits = props.hits;
-    //const Listitems = hits.map((hit) => {
-    //    <li>{hit}</li>;
-    return (
+	//const hits = props.hits;
+	//const Listitems = hits.map((hit) => {
+	//    <li>{hit}</li>;
+	return (
+		<div>
+			<div className='main-nav'>
+				<h3 className='header'>News</h3>
+				<input
+					className='searchInput'
+					type='text'
+					placeholder=' Enter '
+					onChange={handleFilter}
+				/>
+				<div className='SearchIcon'>
+					<SearchIcon />
+				</div>
+			</div>
 
-        <div>
-         <div className='main-nav'>
-           <h3 className='header'>News</h3>
-                <input className='searchInput' type='text' placeholder=' Enter ' onChange={ handleFilter } /> 
-                  <div className='SearchIcon'>
-                    <SearchIcon />
-                   </div>
-          </div>
-            
-      
-            
-        { filterhits.length !== 0 && (
-            <ul >
-            
-                {filterhits.map((hit, index) => {
-                    console.log(hit);
-                    return <li className="hitsResult"
-                     key={index}>{hit.title}
-                     </li>;
-                })}
-                
-            </ul>
-)}
-        </div>
-    );
+			<div className='hitsItem'>
+				{filterhits.length !== 0 && (
+					<ul>
+						{filterhits.map((hit, index) => {
+							//console.log(hit);
+							return (
+								<li className='hitsResult' key={index}>
+									{hit.title}
+								</li>
+							);
+						})}
+					</ul>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default List;
