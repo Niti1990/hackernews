1878148bdf5297f8b6ca5f840cffe0140308ddd1
Saritha Sujith <pillai.sujith.saritha@gmail.com>
2022-02-25 20:06:50 +0100

    added the search value

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -7,14 +7,15 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 	const [news, setNews] = useState([]);
-	// const [searchValue, setSearchValue] = useState([]);
+	//setSearchvalue to whatever is in the input field in List.js
+	const [searchValue, setSearchValue] = useState('react');
 	//console.log(news);
 	useEffect(() => {
 		const fetchNews = async () => {
 			try {
-				const searchValue = 'react';
+				//const searchValue = 'react';
 				const response = await fetch(
-					`http://hn.algolia.com/api/v1/search?query=${setSearchValue}`
+					`http://hn.algolia.com/api/v1/search?query=${searchValue}`
 				);
 				if (response.ok) {
 					const jsonResponse = await response.json();
@@ -29,15 +30,22 @@ function App() {
 			}
 		};
 		fetchNews();
-	}, []);
-	const handleSubmit = (searchValue) => {
-		console.log('hello', searchValue);
+	}, [searchValue]);
+
+	//goal: setSearchValue to input.value
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('target', e.target);
+		console.log('current searchvalue', searchValue);
+		setSearchValue(e.target.searchbar.value);
+		//alert('hi there');
 	};
+
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<Header />
-				<List hits={news} submit={handleSubmit} />
+				<List hits={news} handleSubmit={handleSubmit} />
 			</header>
 		</div>
 	);
--- hackernews/src/components/List.js
+++ hackernews/src/components/List.js
@@ -2,36 +2,38 @@ import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 
 //import {Link} from 'react-router-dom';
-function List({ hits, submit }) {
-	const [filterhits, setFilterhits] = useState([]);
+function List({ hits, handleSubmit }) {
+	// const [filterhits, setFilterhits] = useState([]);
 
-	const handleFilter = (event) => {
-		const searchWord = event.target.value;
-		const newFilter = hits.filter((value) => {
-			return value.title.toLowerCase().includes(searchWord.toLowerCase());
-		});
+	// const handleFilter = (event) => {
+	// 	const searchWord = event.target.value;
+	// 	const newFilter = hits.filter((value) => {
+	// 		return value.title.toLowerCase().includes(searchWord.toLowerCase());
+	// 	});
 
-		setFilterhits(newFilter);
-	};
-	const handleOnSubmit = (event) => {
-		event.preventDefault();
-		const searchWord = event.target.value;
-		submit(searchWord);
-	};
+	// 	setFilterhits(newFilter);
+	// };
+	// const handleOnSubmit = (event) => {
+	// 	event.preventDefault();
+	// 	const searchWord = event.target.value;
+	// 	submit(searchWord);
+	// };
 
 	//const hits = props.hits;
 	//const Listitems = hits.map((hit) => {
 	//    <li>{hit}</li>;
+
 	return (
 		<div>
 			<div className='main-nav'>
 				<h3 className='header'>News</h3>
-				<form onSubmit={handleOnSubmit}>
+				<form onSubmit={handleSubmit}>
 					<input
 						className='searchInput'
 						type='text'
 						placeholder=' Enter '
-						onChange={handleFilter}
+						name='searchbar'
+						// onChange={handleSubmit}
 					/>
 					<button type='submit'>Submit</button>
 					<div className='SearchIcon'>
@@ -41,7 +43,14 @@ function List({ hits, submit }) {
 			</div>
 
 			<div className='hitsItem'>
-				{filterhits.length !== 0 && (
+				{hits.length !== 0 && (
+					<ul>
+						{hits.map((hit) => {
+							return <li>{hit.title}</li>;
+						})}
+					</ul>
+				)}
+				{/* {filterhits.length !== 0 && (
 					<ul>
 						{filterhits.map((hit, index) => {
 							//console.log(hit);
@@ -51,8 +60,7 @@ function List({ hits, submit }) {
 								</li>
 							);
 						})}
-					</ul>
-				)}
+					</ul> */}
 			</div>
 		</div>
 	);
